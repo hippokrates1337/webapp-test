9bd8537b9e0ad9754670c7b59e99c636c0cf0806
hippokrates1337 <44327653+hippokrates1337@users.noreply.github.com>
2023-08-08 22:29:52 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Test app successfully deployed!');
 });
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log('App is listening on port ${PORT}');
 });
