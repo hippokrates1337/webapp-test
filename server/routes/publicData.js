b8a7cc9bfc5699a4d5ac778257d0efeedfc14c8a
hippokrates1337 <j.weber_2003@web.de>
2023-08-13 19:06:12 +0000

    Aggregate data being served

--- server/routes/publicData.js
+++ server/routes/publicData.js
@@ -1,11 +1,30 @@
 import express from 'express';
-import {recreateAggregateTimeSeries, recreateUserDailyData} from '../api/dataAggregation.js';
+import { AggregateTimeSeries, ResourceTypes } from '../database/models.js';
 
 const route = express();
 
-route.get('/', async (req, res) => {
-    //await recreateUserDailyData();
-    await recreateAggregateTimeSeries();
+route.get('/allConsumption', async (req, res) => {
+    let response;
+
+    try {
+        response = await AggregateTimeSeries.find().exec();
+    } catch(error) {
+        console.error(error);
+    }
+    
+    res.status(200).json(response);
+});
+
+route.get('/resourceTypes', async (req, res) => {
+    let response;
+
+    try {
+        response = await ResourceTypes.find().exec();
+    } catch(error) {
+        console.error(error);
+    }
+    
+    res.status(200).json(response);
 });
 
 export default route;
