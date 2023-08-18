e0071f056c6e3992730b204e591f547efc2ebe27
hippokrates1337 <j.weber_2003@web.de>
2023-08-18 07:48:21 +0000

    Continued login form development

--- client/src/views/account/AccountLogin.vue
+++ client/src/views/account/AccountLogin.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="card m-3">
-        <h4 class="card-header">Anmelden</h4>
+        <h4 class="card-header">Als existierender Benutzer anmelden</h4>
         <div class="card-body">
-            <form>
+            <form @submit.prevent="handleSubmit">
                 <div class="form-group">
-                    <label>Benutzername</label>
-                    <input name="username" type="text" class="form-control" />
+                    <label class="form-label">Benutzername</label>
+                    <input v-model="userName" id="username" type="text" class="form-control" required />
                 </div>
-                <button @click="onSubmit">Anmelden</button>
+                <button class="btn" type="submit">Abschicken</button>
             </form>
         </div>
     </div>
 </template>
 
 <script setup>
-    const onSubmit = (values) => {
-        console.log(values);
+    import { ref } from 'vue';
+
+    const userName = ref('');
+
+    const handleSubmit = () => {
+        console.log(userName.value);
     }
 </script>
