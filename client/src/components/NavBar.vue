e5bf98372e9f91297f78385986eed5078fd52eec
hippokrates1337 <j.weber_2003@web.de>
2023-08-25 18:55:19 +0000

    Added dummy page plus routes

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -7,7 +7,15 @@
             </button>
             <div class="navbar-nav">
                 <router-link to="/" class="nav-item nav-link">Start</router-link>
-                <router-link v-show="authStore.user" to="/user" class="nav-item nav-link">Meine Daten</router-link>
+                <div class="nav-item dropdown" v-show="authStore.user">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Meine Daten
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><router-link to="/user" class="nav-item nav-link">Übersicht</router-link></li>
+                        <li><router-link to="/user/consumers" class="nav-item nav-link">Verbraucher verwalten</router-link></li>
+                    </ul>
+                </div>
                 <router-link v-show="!authStore.user" to="/account/login" class="nav-item nav-link">Anmelden</router-link>
                 <router-link v-show="!authStore.user" to="/account/register" class="nav-item nav-link">Registrieren</router-link>
                 <button v-show="authStore.user" @click="authStore.logout()" class="btn btn-link nav-item nav-link">Abmelden</button>
--- client/src/router/userRoutes.js
+++ client/src/router/userRoutes.js
@@ -1,7 +1,12 @@
-import UserLanding from '@/views/user/UserLanding.vue';
+import UserLayout from '@/views/user/UserLayout.vue';
+import UserOverview from '@/views/user/UserOverview.vue';
+import ConsumerList from '@/views/user/ConsumerList.vue'
 
 export default {
     path: '/user',
-    component: UserLanding,
-    children: []
+    component: UserLayout,
+    children: [
+        {path: '', component: UserOverview},
+        {path: 'consumers', component: ConsumerList},
+    ]
 };
