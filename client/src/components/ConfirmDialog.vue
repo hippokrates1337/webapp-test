7ed97398eb271330b023da1f6119ad81a63c2f07
hippokrates1337 <j.weber_2003@web.de>
2023-08-29 15:33:19 +0000

    Implemented confirm dialog

---
+++ client/src/components/ConfirmDialog.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="modal fade" tabindex="-1" id="confirmdialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">
+                        <slot name="title"></slot>
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <slot name="body"></slot>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="props.callback">Bestätigen</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        callback: Function
+    });
+</script>
