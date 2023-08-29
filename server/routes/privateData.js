5ecd0f04ac799b82de0b4f4f6b3b2ec21c353aeb
hippokrates1337 <j.weber_2003@web.de>
2023-08-29 15:05:52 +0000

    Code refactored

--- server/routes/privateData.js
+++ server/routes/privateData.js
@@ -9,12 +9,12 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
 
-    if(token == null) return res.sendStatus(401);
+    if(token == null) return res.status(401);
 
     jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
         if(err) {
             console.log(err);
-            return res.sendStatus(403);
+            return res.status(403);
         }
 
         req.user = user;
@@ -22,230 +22,154 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
-route.get('/timeSeries/:id', authenticateToken, async (req, res) => {
-    console.log('PrivateData.js - Received request for consumer-level time series');
-    let timeseries;
+const executeOperation = async (req, res, operation) => {
+    let result;
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
+    if(res.status == 401 || res.status == 403) {
+        res.end();
     } else {
         try {
-            const consumers = await Consumer.find({
-                user: req.params.id
-            }).exec();
-            timeseries = await ConsumerTimeSeries.find({
-                consumer: {
-                    '$in': consumers.map((elem) => elem._id)
-                }
-            }).exec();
+            result = await operation();
         } catch(error) {
             console.error(error);
             res.status(500).send('Error accessing the database!' + error);
         }
 
-        if(timeseries) {
-            res.status(200).json(timeseries);
+        if(result) {
+            res.status(200).json(result);
         }
     }
+};
+
+route.get('/timeSeries/:id', authenticateToken, async (req, res) => {
+    console.log('PrivateData.js - Received request for consumer-level time series');
+
+    await executeOperation(req, res, async () => {
+        const consumers = await Consumer.find({
+            user: req.params.id
+        }).exec();
+        const timeseries = await ConsumerTimeSeries.find({
+            consumer: {
+                '$in': consumers.map((elem) => elem._id)
+            }
+        }).exec();
+
+        return timeseries;
+    });
 });
 
 route.get('/consumers/:id', authenticateToken, async (req, res) => {
     console.log('PrivateData.js - Received request for consumer data');
-    let consumers;
+    
+    await executeOperation(req, res, async () => {
+        const consumers = await Consumer.find({
+            user: req.params.id
+        }).exec();
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
-    } else {
-        try {
-            consumers = await Consumer.find({
-                user: req.params.id
-            }).exec();
-        } catch(error) {
-            console.error(error);
-            res.status(500).send('Error accessing the database!' + error);
-        }
-
-        if(consumers) {
-            res.status(200).json(consumers);
-        }
-    }
+        return consumers;
+    });
 });
 
 route.patch('/consumers/:id', authenticateToken, async (req, res) => {
     console.log('PrivateData.js - Received request to update data on an existing consumer');
-    let consumer;
+    
+    await executeOperation(req, res, async () => {
+        const consumer = await Consumer.findOneAndUpdate(
+            {
+                user: req.params.id,
+                _id: req.body._id
+            }, 
+            {
+                ...req.body,
+                lastUpdate: new Date()
+            }, 
+            {
+                new: true
+            }).exec();
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
-    } else {
-        try {
-            consumer = await Consumer.findOneAndUpdate(
-                {
-                    user: req.params.id,
-                    _id: req.body._id
-                }, 
-                {
-                    ...req.body,
-                    lastUpdate: new Date()
-                }, 
-                {
-                    new: true
-                }).exec();
-        } catch(error) {
-            console.error(error);
-            res.status(500).send('Error accessing the database!' + error);
-        }
-
-        if(consumer) {
-            res.status(200).json(consumer);
-        }
-    }
+        return consumer;
+    });
 });
 
 route.post('/consumers/:id', authenticateToken, async (req, res) => {
     console.log('PrivateData.js - Received request to create a new consumer');
-    let consumer;
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
-    } else {
-        try {
-            consumer = new Consumer({
-                ... req.body,
-                user: req.params.id,
-                createdOn: new Date(),
-                lastUpdate: new Date()
-            });
+    await executeOperation(req, res, async () => {
+        let consumer = new Consumer({
+            ... req.body,
+            user: req.params.id,
+            createdOn: new Date(),
+            lastUpdate: new Date()
+        });
 
-            consumer = await consumer.save();
-        } catch(error) {
-            console.error(error);
-            res.status(500).send('Error accessing the database!' + error);
-        }
-
-        if(consumer) {
-            res.status(200).json(consumer);
-        }
-    }
+        consumer = await consumer.save();
+        return consumer;
+    });
 });
 
 route.get('/datapoints/:id', authenticateToken, async (req, res) => {
     console.log('PrivateData.js - Received request for user data points');
-    let datapoints;
+    
+    await executeOperation(req, res, async () => {
+        const consumers = await Consumer.find({
+            user: req.params.id
+        }).exec();
+        const datapoints = await Datapoint.find({
+            consumer: {
+                '$in': consumers.map((elem) => elem._id)
+            }
+        }).exec();
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
-    } else {
-        try {
-            const consumers = await Consumer.find({
-                user: req.params.id
-            }).exec();
-            datapoints = await Datapoint.find({
-                consumer: {
-                    '$in': consumers.map((elem) => elem._id)
-                }
-            }).exec();
-        } catch(error) {
-            console.error(error);
-            res.status(500).send('Error accessing the database!' + error);
-        }
-
-        if(datapoints) {
-            res.status(200).json(datapoints);
-        }
-    }
+        return datapoints;
+    });
 });
 
 route.patch('/datapoints/:id', authenticateToken, async (req, res) => {
     console.log('PrivateData.js - Received request to update an existing datapoint');
-    let datapoint;
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
-    } else {
-        try {
-            datapoint = await Datapoint.findOneAndUpdate(
-                {
-                    _id: req.body._id
-                }, 
-                {
-                    ...req.body,
-                    lastUpdate: new Date()
-                }, 
-                {
-                    new: true
-                }).exec();
-        } catch(error) {
-            console.error(error);
-            res.status(500).send('Error accessing the database!' + error);
-        }
+    await executeOperation(req, res, async () => {
+        const datapoint = await Datapoint.findOneAndUpdate(
+            {
+                _id: req.body._id
+            }, 
+            {
+                ...req.body,
+                lastUpdate: new Date()
+            }, 
+            {
+                new: true
+            }).exec();
 
-        if(datapoint) {
-            res.status(200).json(datapoint);
-        }
-    }
+            return datapoint;
+    });
 });
 
 route.post('/datapoints/:id', authenticateToken, async (req, res) => {
     console.log('PrivateData.js - Received request to create a new datapoint');
-    let datapoint;
+    
+    await executeOperation(req, res, async () => {
+        let datapoint = new Datapoint({
+            ... req.body,
+            createdOn: new Date(),
+            lastUpdate: new Date()
+        });
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
-    } else {
-        try {
-            datapoint = new Datapoint({
-                ... req.body,
-                createdOn: new Date(),
-                lastUpdate: new Date()
-            });
-
-            datapoint = await datapoint.save();
-        } catch(error) {
-            console.error(error);
-            res.status(500).send('Error accessing the database!' + error);
-        }
-
-        if(datapoint) {
-            res.status(200).json(datapoint);
-        }
-    }
+        datapoint = await datapoint.save();
+        return datapoint;
+    });
 });
 
 route.delete('/datapoints/:id', authenticateToken, async (req, res) => {
     console.log('PrivateData.js - Received request to delete an existing datapoint');
 
-    if(res.status == 401) {
-        res.send('Access denied - No bearer token presented!');
-    } else if(res.status == 403) {
-        res.send('Access denied - Invalid bearer token!');
-    } else {
-        try {
-            await Datapoint.findOneAndDelete(
-                {
-                    _id: req.body.id
-                }).exec();
-        } catch(error) {
-            console.error(error);
-            res.status(500).send('Error accessing the database!' + error);
-        }
+    await executeOperation(req, res, async () => {
+        await Datapoint.findOneAndDelete(
+            {
+                _id: req.body.id
+            }).exec();
 
-        res.status(200).send('Successfully deleted!');
-    }
+        return {message: 'Successfully deleted!'};
+    });
 });
 
 export default route;
